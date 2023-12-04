d48286910299e4e528b14fadaae8416bfe6d0dac
Luiza Costa <luizamachadocn@outlook.com>
2023-12-04 13:18:17 -0300

    altera avalia capacidade maxima

--- main.cpp
+++ main.cpp
@@ -219,24 +219,7 @@ float calcularDistanciaNos(No *no1, No *no2)
 
 bool avaliarCapacidadeMaximaRota(Rota *rota, Rota *novaRota)
 {
-    int capMax = rota->getCapacidade();
-    int capacidadeAtual = 0;
-
-    No *noNav = rota->getPrimeiroElemento();
-    while (noNav != NULL)
-    {
-        capacidadeAtual += noNav->getPeso();
-        noNav = noNav->getProxNo();
-    }
-
-    noNav = novaRota->getPrimeiroElemento();
-    while (noNav != NULL)
-    {
-        capacidadeAtual += noNav->getPeso();
-        noNav = noNav->getProxNo();
-    }
-
-    return (capacidadeAtual <= capMax);
+    return (rota->getPesoTotal() + novaRota->getPesoTotal()) <= Grafo->getCapacidadeMaxima();
 }
 
 Rota *mesclarRotas(Rota *rota1, Rota *rota2)
